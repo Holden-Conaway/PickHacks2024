433b00c0b399f68a0f26a81383a0d4bd96a2ccc6
Kevin Lai <zlnh4@umsystem.edu>
2024-04-06 16:15:56 -0500

    add controller logic

---
+++ _controller.js
@@ -0,0 +1,2 @@
+<script src="/node_modules/leapjs/leap-1.1.1.js"></script>
+<script src="/controller.js"></script>
---
+++ controller.js
@@ -0,0 +1,30 @@
+
+console.log("HELLO")
+
+window.hands = {
+  left: { 
+    visible: false,
+    numFingers: 0,
+  },
+  right: {
+    visible: false,
+    numFingers: 0,
+  }
+}
+
+Leap.loop({
+  frame: function(frame) {
+   window.hands.right.visible = false
+   window.hands.left.visible = false
+  },
+  hand: function(hand) {
+    var thishand = window.hands[hand.type]
+    thishand.visible = true
+    thishand.numFingers = 0
+    for (let finger of hand.fingers) {
+      thishand.numFingers += finger.extended
+    }    
+  }
+})
+
+
