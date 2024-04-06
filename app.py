4f0f5c2620ffce65571959c9868715446b6e6549
Epicroce <sethshadeRS@gmail.com>
2024-04-06 15:02:28 -0500

    Added the new-password route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import flask
+import hashlib
 
 app = flask.Flask(__name__)
+old_password = "00000"
 
 @app.route("/")
 def index():
@@ -9,6 +11,13 @@ def index():
 # Define a route that serves static web pages
 
 # Define a route that serves /new-password
+@app.route("/new-password", methods=['POST'])
+def new_password():
+    new_password = flask.request.form.get('new-password')
+    new_password_hash = hashlib.sha256(new_password.encode()).hexdigest()
+    print(new_password_hash)
+
+
 ## IT is a post request. JSON as body. Store new password as crypto hash (SHA256)
 # Define a route that serves /verify-password
 
