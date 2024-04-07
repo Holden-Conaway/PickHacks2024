b2b86fa5e61b9b10d01347020b5e8237f3e0e345
Kevin Lai <zlnh4@umsystem.edu>
2024-04-06 19:33:38 -0500

    update

--- controller.js
+++ controller.js
@@ -9,13 +9,15 @@ window.hands = {
   right: {
     visible: false,
     numFingers: 0,
-  }
+  },
+  altConfirm: false
 }
 
 Leap.loop({
   frame: function(frame) {
    window.hands.right.visible = false
    window.hands.left.visible = false
+   window.hands.altConfirm = false
   },
   hand: function(hand) {
     var thishand = window.hands[hand.type]
@@ -24,7 +26,19 @@ Leap.loop({
     for (let finger of hand.fingers) {
       thishand.numFingers += finger.extended
     }
-    console.log(thishand)    
+    
+   
+    if (hand.type == "left" 
+        && hand.fingers[0].extended 
+        && hand.fingers[4].extended
+        && ! hand.fingers[1].extended
+        && ! hand.fingers[2].extended
+        && ! hand.fingers[3].extended
+    ) {
+      window.hands.altConfirm = true
+      console.log("alt confirm")
+    }
+
   }
 })
 
